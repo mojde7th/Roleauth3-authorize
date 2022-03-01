e6a3ac3d1d7d4636221dd806b0bf565c90d1a8f1
98912 <98912@DESKTOP-A33184V>
2022-03-01 10:18:49 +0330

    Add project files.

---
+++ Roleauth3/Controllers/MelinaftController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Roleauth3.Controllers
+{
+    public class MelinaftController : Controller
+    {
+        [Authorize(Roles="Melinaft")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
